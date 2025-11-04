1ed9b182ce591baec7de5ca920780658484819c9
Humayun <humayunhoda@gmail.com>
2025-11-04 12:42:06 +0530

    vh

--- assets/app.js
+++ assets/app.js
@@ -53,7 +53,7 @@ function searchNews() {
 
             if (found) {
                 
-                window.location.href = `https://news.inbondz.com/news/${matchedItem.news}.html`;
+                window.location.href = `https://news.inbondz.com/news/${matchedItem.news}`;
 
             }
             else {
